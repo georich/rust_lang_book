b8d6b6846bea398f9b252844cc58a6929b4dc2ec
George Richards <34480725+georich@users.noreply.github.com>
2018-05-06 22:06:03 +0100

    Cargo workspace understanding

---
+++ add/adder/src/main.rs
@@ -0,0 +1,9 @@
+extern crate rand;
+extern crate add_one;
+extern crate add_two;
+
+fn main() {
+    let num = 10;
+    println!("Hello world! {} plus one is {}!", num, add_one::add_one(num));
+    println!("And now, {} plus two is {}!", num, add_two::add_two(num));
+}
